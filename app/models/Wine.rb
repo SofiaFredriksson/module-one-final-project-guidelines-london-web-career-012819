6990669d59b9d483e5eda3a0aaafd91160ca0b7d
stubrew24 <sewelly@gmail.com>
2019-02-11 17:45:22 +0000

    latest

--- app/models/Wine.rb
+++ app/models/Wine.rb
@@ -2,4 +2,8 @@ class Wine < ActiveRecord::Base
   has_many :reviews
   belongs_to :winery
   has_many :tasters, through: :reviews
+
+  def self.top_ten
+    ##
+  end
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,15 @@
 require_relative '../config/environment'
 
 
+puts "Enter your name."
+username = gets.chomp
+user = Taster.find_by(name: username)
 
-puts "HELLO WORLD"
+puts "Show reviews? [y/n]"
+input = gets.chomp
+
+if input == "y"
+  user.reviews.each do |r|
+    puts r.description
+  end
+end
